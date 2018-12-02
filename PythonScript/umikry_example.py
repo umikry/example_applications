d52921ef6cad36d382803884930b0d2e2d27137d
Fabian Bormann <fabian_mb@gmx.de>
2018-12-02 02:48:29 +0100

    implement basic toolchain

--- PythonScript/umikry_example.py
+++ PythonScript/umikry_example.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import json
 import cv2
 
 sys.path.append('../../umikry-core/')
@@ -12,12 +13,20 @@ parser.add_argument('--detection', default='CAFFE',
                     help='umikry detection method (HAAR, CAFFE, CNN)')
 parser.add_argument('--transformation', default='GAN',
                     help='umikry transformation method (GAN, AUTOENCODER, BLUR)')
+parser.add_argument('--face_store', help='output file path')
 parser.add_argument('--output', default='output.jpg',
                     help='output file path')
 
 args = parser.parse_args()
-
 image = cv2.imread(args.image)
-image = umikry(image, detection=args.detection, transformation=args.transformation)
+
+if args.face_store:
+    with open(args.face_store, 'r') as json_file:
+        familar_faces = json.load(json_file)
+else:
+    familar_faces = None
+
+image = umikry(image, detection=args.detection, transformation=args.transformation,
+               familar_faces=familar_faces)
 
 cv2.imwrite(args.output, image)
